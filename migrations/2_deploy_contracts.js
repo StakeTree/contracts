ee15d5dd9083496d95b188bdd24af29337fec502
Niel de la Rouviere <nieldlr@gmail.com>
2017-09-20 12:31:12 +0200

    Rinkeby deploys

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
 var StakeTreeMVP = artifacts.require("./StakeTreeMVP.sol");
 
+const nowUnix = new Date().getTime()/1000 - 3000;
+const nowParsed = parseInt(nowUnix.toFixed(0), 10);
+
+// Deploy config
+const config = {
+  beneficiaryAddress: "0x905d7605f2c79d4dad2c42586f62bb4bae6dfdb3",
+  withdrawalPeriod: 1209600, // 2 weeks
+  startTime: nowParsed,
+  sunsetWithdrawalPeriod: 5184000 // 2 months
+};
+
 module.exports = function(deployer) {
-  deployer.deploy(StakeTreeMVP);
+  deployer.deploy(StakeTreeMVP, config.beneficiaryAddress, config.withdrawalPeriod, config.startTime, config.sunsetWithdrawalPeriod);
 };
--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x905d7605f2c79d4dad2c42586f62bb4bae6dfdb3", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
