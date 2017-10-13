e20d0dda12132c317a62166fedb48b26298ced4b
Niel de la Rouviere <nieldlr@gmail.com>
2017-10-13 10:10:18 +0200

    Add localEngine

--- truffle.js
+++ truffle.js
@@ -3,14 +3,15 @@ const config = require('./localConfig.js');
 
 const mnemonic = config.mnemonic;
 
+const localEngine = new HDWalletProvider(mnemonic, "http://localhost:8545");
 const rinkebyEngine = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io");
 const mainnetEngine = new HDWalletProvider(mnemonic, "https://mainnet.infura.io");
 
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      provider: localEngine,
+      address: localEngine.getAddress(),
       network_id: "*" // Match any network id
     },
     mainnet: {
