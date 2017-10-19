f63883f50b0beab2692b21adc6cc158f26da2cad
Niel de la Rouviere <nieldlr@gmail.com>
2017-10-19 15:59:16 +0200

    Create a contract factory

---
+++ contracts/StakeTreeMVPFactory.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.11;
+import './StakeTreeMVP.sol';
+
+contract StakeTreeMVPFactory {
+  uint public contractCount;
+  mapping(address => address) public contracts;
+
+  function newContract(
+    address beneficiaryAddress, 
+    uint withdrawalPeriodInit, 
+    uint withdrawalStart, 
+    uint sunsetWithdrawPeriodInit,
+    uint minimumFundingAmountInit ) public returns (address newAddress) {
+
+    StakeTreeMVP mvp = new StakeTreeMVP(
+      beneficiaryAddress, 
+      withdrawalPeriodInit, 
+      withdrawalStart, 
+      sunsetWithdrawPeriodInit,
+      minimumFundingAmountInit
+    );
+
+    contracts[msg.sender] = mvp;
+    contractCount += 1;
+
+    return mvp;
+  }
+
+  function getContractAddress() returns (address) {
+    return contracts[msg.sender];
+  }
+}
